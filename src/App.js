b835bb0ab8f76713ce3000b5172e110a8ed02356
Camsd <codecos.info@gmail.com>
2023-06-07 07:41:44 -0500

    background video

--- src/App.js
+++ src/App.js
@@ -5,8 +5,24 @@ import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 //clock css
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 
+//import video
+import VideoBg from '../src/assets/video.mp4';
+
+//import css
+import './styles.css';
+
 const App = () => {
-  return <div>react app</div>;
+  return <section className='page'>
+    {/* overlay */}
+    <div className='overlay'></div>
+    {/* video */}
+    <video src={VideoBg} autoPlay loop muted></video>
+    {/* content */}
+    <div className="page__content">
+      <h1>Launching Soon</h1>
+      <h3></h3>
+    </div>
+  </section>;
 };
 
 export default App;
