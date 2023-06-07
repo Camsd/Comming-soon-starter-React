62172d1ccbbe2501c303fa31ccd710fccfdc9470
Camsd <codecos.info@gmail.com>
2023-06-07 12:11:22 -0500

    button

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React from "react";
 
 //import clock
-import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
 //clock css
-import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
 
 //import video
-import VideoBg from '../src/assets/video.mp4';
+import VideoBg from "../src/assets/video.mp4";
 
 //import css
-import './styles.css';
+import "./styles.css";
 
 const App = () => {
-  return(
-  <section className='page'>
-    {/* overlay */}
-    <div className='overlay'></div>
-    {/* video */}
-    <video src={VideoBg} autoPlay loop muted></video>
-    {/* content */}
-    <div className="page__content">
-      <h1>Launching Soon</h1>
-      <h3>Leave your email and we'll let know once the site goes live.</h3>
-      {/* clock */}
-      <FlipClockCountdown to={new Date().getTime() + 24 * 3600 * 1000 + 5000} className='flip-clock' />;
-    </div>
-  </section>
+  return (
+    <section className="page">
+      {/* overlay */}
+      <div className="overlay"></div>
+      {/* video */}
+      <video src={VideoBg} autoPlay loop muted></video>
+      {/* content */}
+      <div className="page__content">
+        <h1>Launching Soon</h1>
+        <h3>Leave your email and we'll let know once the site goes live.</h3>
+        {/* clock */}
+        <FlipClockCountdown
+          to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
+          className="flip-clock"
+        />
+        {/* button */}
+        <button className="btn">Notify Me</button>
+      </div>
+    </section>
   );
 };
 
