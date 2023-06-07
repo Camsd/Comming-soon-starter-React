ef7e890fa5dd3eadce6e78f951cd5d715fe140a0
Camsd <codecos.info@gmail.com>
2023-06-07 07:15:32 -0500

    add dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+//import clock
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
+//clock css
+import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+
 const App = () => {
   return <div>react app</div>;
 };
